a1b03f136bdfbeb870af759b68ced2e22d902490
Tanu singhal <tanusinghal958@gmail.com>
2021-04-08 13:44:43 +0530

    add landing page

---
+++ exercises/split-landing-page/split-landing-page.js
@@ -0,0 +1,9 @@
+const left = document.querySelector(".split-left")
+const right = document.querySelector(".split-right")
+const container = document.querySelector(".main-container")
+
+left.addEventListener('mouseenter', () => main-container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => main-container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter', () => main-container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => main-container.classList.remove('hover-right'))
