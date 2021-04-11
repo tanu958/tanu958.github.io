6fb8ec25cc72f8a0d6ab156e7897888446e7b28c
Tanu singhal <tanusinghal958@gmail.com>
2021-04-11 22:30:03 +0530

    Split landing

--- exercises/split-landing-page/split-landing-page.js
+++ exercises/split-landing-page/split-landing-page.js
@@ -1,9 +1,9 @@
-const left = document.querySelector(".split-left")
-const right = document.querySelector(".split-right")
-const container = document.querySelector(".main-container")
+const left = document.querySelector(".left")
+const right = document.querySelector(".right")
+const container = document.querySelector(".container")
 
-left.addEventListener('mouseenter', () => main-container.classList.add('hover-left'))
-left.addEventListener('mouseleave', () => main-container.classList.remove('hover-left'))
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
 
-right.addEventListener('mouseenter', () => main-container.classList.add('hover-right'))
-right.addEventListener('mouseleave', () => main-container.classList.remove('hover-right'))
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
